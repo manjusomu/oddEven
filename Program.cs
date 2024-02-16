820e8fe6d9bcf2490ea30b928068d59c4c0541f1
manjusomu <manjusomu2001@gmail.com>
2024-02-16 09:34:45 +0530

    Check

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConApp;
+
+Console.WriteLine("Welcome !");
+Console.WriteLine(" ***Even number****");
+CheckNum.EvenNum();
+Console.WriteLine("***odd Number***");
+CheckNum.OddNum();
